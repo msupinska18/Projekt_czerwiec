d8aa308dbaeb481a2f507b426abb6a119db8076d
msupinska18 <msupinska18@gmail.com>
2025-06-14 18:42:28 +0200

    .gitignore

--- test_coccodrillo.py
+++ test_coccodrillo.py
@@ -15,13 +15,5 @@ def test_open_coccodrillo(driver):
     time.sleep(5)
     assert "Coccodrillo" in driver.title, "Strona nie otworzyła się poprawnie"
 
-#def test_go_to_niemowlak(driver):
- #   driver.get("https://pl.coccodrillo.eu/")
-    
-    # Znalezienie zakładki "Niemowlak 0-2 lat" i kliknięcie
-  #  niemowlak_tab = driver.find_element(By.LINK_TEXT, "Niemowlak 0-2 lat")
-   # niemowlak_tab.click()
 
-    # Sprawdzenie, czy strona została poprawnie otwarta
-    #assert "Niemowlak" in driver.page_source, "Zakładka Niemowlak się nie otworzyła"
 
